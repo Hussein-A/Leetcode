4138533fa9dfa6e88545dc02413da896fa392442
Hussein-A <43759836+Hussein-A@users.noreply.github.com>
2019-03-14 18:09:18 -0400

    Add problem 141 Soln
    Added the solution to problem 141, linked list cycle.

--- SolvedLeetcode.cpp
+++ SolvedLeetcode.cpp
@@ -726,6 +726,32 @@ public:
 };
 */
 
+/*
+Problem 141. Linked List Cycle
+Difficulty: Easy
+Description: Given a linked list, determine if it has a cycle in it.
+To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) 
+in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+
+class Solution {
+public:
+	bool hasCycle(ListNode *head) {
+		if (head == NULL ||head->next == NULL) return false;
+
+		ListNode* slow = head;
+		ListNode* fast = head;
+		while(fast!= NULL && fast->next != NULL){
+			fast= fast->next;
+			if (fast == slow) return true;
+			else {slow = slow->next; fast = fast->next;}
+		}
+		return false;
+
+	}
+};
+
+*/
+
 int main() {
 
 }
