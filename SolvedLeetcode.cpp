810b3e972a35eae0ddebc4fa8b2e1272f603bb7b
Hussein Ahmad <hussein.tec101@gmail.com>
2019-03-20 12:13:29 -0400

    Add 116, Remove pointless "#include"
    Added solution to problem 116, Medium
    Populating Next Right Pointers in Each Node
    Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
    
    Also removed the include statements as this is not meant to be a stand alone program, rather contains the solutions to be placed into leetcode.

--- SolvedLeetcode.cpp
+++ SolvedLeetcode.cpp
@@ -1,8 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-using std::cout;
+
 
 /*  Problem 160. Intersection of Two Linked Lists
 	Difficulty: Easy
@@ -767,6 +763,43 @@ public:
 		return false;
 	}
 };
+*/
+
+/*
+Problem 231. Populating Next Right Pointers in Each Node
+Difficulty: Medium
+Decription: You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. 
+The binary tree has the following definition: (See attached readme for node definition, added next pointer only difference)
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+Initially, all next pointers are set to NULL.
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (root == NULL) return root;
+        else if (root->left == NULL) {root->next = NULL; return root;}
+        root->next = NULL;
+        
+        queue<Node*> curr_level;
+        queue<Node*> next_level;
+        next_level.push(root->left); next_level.push(root->right);
+        
+        while(!next_level.empty()){
+            curr_level.swap(next_level); //now jumping into next level
+            Node* temp;
+            
+            while(!curr_level.empty()){
+                temp = curr_level.front(); curr_level.pop();
+                if (curr_level.empty()) temp->next = NULL;
+                else temp->next = curr_level.front();
+                //if left child exists then so must right (perfect binary tree)
+                if (temp->left) {next_level.push(temp->left); next_level.push(temp->right);}
+            }
+        }
+        return root;
+    }
+};
+
 */
 
 int main() {
