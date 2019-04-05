586c3233fd12ab0152ce957d6d6ba25c4a640858
Hussein Ahmad <hussein.tec101@gmail.com>
2019-04-05 16:03:15 -0400

    Add solutions 74, 240
    added solutions to Search a Matrix I and II

--- SolvedLeetcode.cpp
+++ SolvedLeetcode.cpp
@@ -1298,6 +1298,67 @@ public:
 };
 */
 
+/*
+Problem 74. Search a 2D Matrix
+Difficulty: Medium
+Description: Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+   * Integers in each row are sorted from left to right.
+   * The first integer of each row is greater than the last integer of the previous row.
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        const int&  row_size= matrix.size();
+        if (row_size == 0 ) return false;
+        const int&  col_size= matrix[0].size();
+        if (col_size == 0) return false;
+        
+        
+        int row_count = 0;
+        vector<int> curr_row = matrix[row_count];
+        while (row_count < row_size){
+            curr_row = matrix[row_count];
+            if (target <= curr_row[col_size-1]) return binary_search(curr_row.begin(), curr_row.end(), target);
+            ++row_count;
+        }
+        return false;
+    }
+};
+*/
+
+/*
+Problem 240. Search a 2D Matrix II
+Difficulty: Medium
+Description: Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+    * Integers in each row are sorted in ascending from left to right.
+    * Integers in each column are sorted in ascending from top to bottom.
+    
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        const int& row_size = matrix.size();
+        if (row_size == 0) return false;
+        const int& col_size = matrix[0].size();
+        if (col_size == 0) return false;
+        
+        
+        int row = row_size-1;
+        int col = 0;
+        int* curr_val= &matrix[row][col];
+        while (row >=0 && col < col_size){
+            curr_val = &matrix[row][col];
+            if (*curr_val == target) return true;
+            else if (target > *curr_val) ++col;
+            else --row;
+        }
+        return false;
+        
+    }
+};
+*/
 int main() {
 
 }
