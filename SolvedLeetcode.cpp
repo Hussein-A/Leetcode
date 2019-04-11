70a390019453f3a0725dfa62ec75331e20214900
Hussein Ahmad <hussein.tec101@gmail.com>
2019-04-10 23:57:45 -0400

    add 104 solution

--- SolvedLeetcode.cpp
+++ SolvedLeetcode.cpp
@@ -1496,6 +1496,41 @@ public:
 };
 */
 
+/*
+Problem 104. Maximum Depth of Binary Tree
+Difficulty: Easy
+Description: Given a binary tree, find its maximum depth.
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+Note: A leaf is a node with no children.
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        //do in level traversal        
+        //initialize
+        queue<TreeNode*> curr_level;
+        queue<TreeNode*> next_level;
+        
+        if (!root) return 0;
+        else next_level.push(root);
+        int depth = 0;
+        
+        while (!next_level.empty()){
+            swap(curr_level, next_level);
+            cout << "1";
+            ++depth;
+            while (!curr_level.empty()){
+                TreeNode*& node = curr_level.front();
+                if (node->left) next_level.push(node->left);
+                if (node->right) next_level.push(node->right);
+                curr_level.pop();
+            }
+        }
+        return depth;
+    }
+};
+*/
+
 int main() {
 
 }
