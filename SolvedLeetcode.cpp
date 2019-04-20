46d40d20c576672b370178b40dd7863be6770d92
Hussein Ahmad <hussein.tec101@gmail.com>
2019-04-19 23:29:11 -0400

    add 344 solution

--- SolvedLeetcode.cpp
+++ SolvedLeetcode.cpp
@@ -1712,6 +1712,35 @@ public:
 };
 */
 
+/*
+Problem 344. Reverse String
+Difficulty: Easy
+Description: Write a function that reverses a string. The input string is given as an array of characters char[].
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+You may assume all the characters consist of printable ascii characters.
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        
+        if (s.size()!=0){
+            char temp1;
+            int size = s.size()-1;
+            int j;
+            int i = s.size()-1;
+            while (i>(s.size()-1)/2){
+                j = size-i;
+                temp1 = s[j];
+                s[j]=s[i];
+                s[i] =temp1;
+                --i;
+        }
+        }
+        
+    }
+};
+*/
+
 int main() {
 
 }
