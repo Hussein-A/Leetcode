12d99610b7a844f475da5dee5bf9e645218cf41c
Hussein Ahmad <hussein.tec101@gmail.com>
2019-04-20 22:21:24 -0400

    add 541 solution

--- SolvedLeetcode.cpp
+++ SolvedLeetcode.cpp
@@ -1740,6 +1740,28 @@ public:
     }
 };
 */
+/*
+Problem 541. Reverse String II
+Difficulty: Easy
+Description:
+Given a string and an integer k, you need to reverse the first k characters for every 2k characters counting 
+from the start of the string. If there are less than k characters left, reverse all of them. 
+If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and left the other as original. 
+
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        
+        for (int i = 0; 2*k*i < s.size(); ++i){
+            auto first = s.begin()+2*k*i;
+            auto last = first+k > s.end() ? s.end() : first+k;
+            reverse(first, last);
+        }
+        return s;
+    }
+};
+
+*/
 
 int main() {
 
